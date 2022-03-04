d6bf1c09a26f7e27b70a7e828a3466872729a3d9
Gideon Tong <gideon@gideontong.com>
2022-03-04 11:51:49 -0800

    Robot that doesn't work

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,14 +1,11 @@
-/* Photoresistor Code UCSD ECE 5 Lab 4 */
-/* Variables for Light Sensors*/
-
-#define totalPhotoResistors 7 // Defining how many photoresistors are used, modify this if more or less are used
-
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
 #define LED_PIN 13
-
+#define BLACK_PIN 22
+#define WHITE_PIN 24
+#define PHOTORESISTOR_COUNT 7
+#define CALIBRATION_COUNT 15
 #define M_FORWARD 0
 #define M_BACKWARD 1
 #define M_LEFT 2
@@ -22,36 +19,28 @@ Adafruit_DCMotor *Motor2 = AFMS.getMotor(2);
 int M1Sp = 60;
 int M2Sp = 60;
 
-// Initialize Photo Resistor Array
-int LDR[totalPhotoResistors]; // Since there are 7 photoresistors, we will number them from 0 to 6 (recall array index start with 0)
+int LDR[PHOTORESISTOR_COUNT];
 
-// ************************************************************************************************* //
-/**
- * Function to read photo resistors
- */
-void ReadPhotoResistors() {
-  int photoResistorCounter = 0;
-  // looping through analog pins A8 to A14 and storing their values into our LDR array
+void readPhotoresistors() {
+  int photoresistorCounter = 0;
   for (byte pin = A8; pin <= A14; pin++) {
-    LDR[photoResistorCounter] = analogRead(pin);
-    photoResistorCounter++;
+    LDR[photoresistorCounter] = analogRead(pin);
+    photoresistorCounter++;
     delay(2);
   }
 }
 
-// ************************************************************************************************* //
-/**
- * Function to print values of interest
- */
-void Print() {
-  for (int i = 0; i < totalPhotoResistors; i++) {
-    // Printing the photo resistor reading values one by one
+void debugPhotoresistors() {
+  for (int i = 0; i < PHOTORESISTOR_COUNT; i++) {
     Serial.print(LDR[i]);
     Serial.print(" ");
   }
-  Serial.println(); // This just prints a new line, allowing the next set of readings to be on a new line
+  Serial.println();
 
-  delay(200); //just here to slow down the output for easier reading if desired
+  delay(200);
+}
+
+void calibratePhotoresistors() {
 }
 
 /**
@@ -62,8 +51,8 @@ void Print() {
  * @param direction: the direction we want the motor to move in
  */
 void runMotorAtSpeed(Adafruit_DCMotor *motor, int speed, int direction) {
-   motor->setSpeed(speed);
-   motor->run(direction);
+  motor->setSpeed(speed);
+  motor->run(direction);
 }
 
 void moveDirection(int direction) {
@@ -92,19 +81,23 @@ void moveDirection(int direction) {
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   AFMS.begin();
   pinMode(LED_PIN, OUTPUT);
+  pinMode(WHITE_PIN, OUTPUT);
+  pinMode(BLACK_PIN, OUTPUT);
 
   Serial.println("Ready!");
 }
 
-void loop()
-{
-    ReadPhotoResistors(); // Read photoresistors and map to 0-100 based on calibration
-
-  Print(); // Print values to serial monitor
+void loop() {
+  digitalWrite(WHITE_PIN, HIGH);
+  digitalWrite(BLACK_PIN, HIGH);
+  runMotorAtSpeed(Motor1, M1Sp, FORWARD);
+  runMotorAtSpeed(Motor2, M2Sp, FORWARD);
+  readPhotoresistors();
+  debugPhotoresistors();
+  delay(0.5);
 }
